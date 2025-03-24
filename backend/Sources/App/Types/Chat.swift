32993d4a874d9203187a6838722a2c84a1c8e90a
monadierickx <126071495+monadierickx@users.noreply.github.com>
2025-03-24 18:47:26 +0100

    tiny HB app refactor

---
+++ backend/Sources/App/Types/Chat.swift
@@ -0,0 +1,30 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Foundation Models Playground open source project
+//
+// Copyright (c) 2025 Amazon.com, Inc. or its affiliates
+//                    and the Swift Foundation Models Playground project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift Foundation Models Playground project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+import Hummingbird
+import SwiftBedrockTypes
+
+extension Message: ResponseCodable {}
+
+struct ChatInput: Codable {
+    let prompt: String
+    let history: [Message]
+}
+
+struct ChatOutput: ResponseCodable {
+    let reply: String
+    let history: [Message]
+}
---
+++ backend/Sources/App/Types/ImageGeneration.swift
@@ -0,0 +1,59 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Foundation Models Playground open source project
+//
+// Copyright (c) 2025 Amazon.com, Inc. or its affiliates
+//                    and the Swift Foundation Models Playground project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift Foundation Models Playground project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+import Hummingbird
+import SwiftBedrockTypes
+
+extension ImageGenerationOutput: ResponseCodable {}
+
+struct ImageGenerationInput: Codable {
+    let prompt: String
+    let nrOfImages: Int?
+    let referenceImage: Data?
+    let similarity: Double?
+
+    init(
+        prompt: String,
+        nrOfImages: Int? = 1,
+        referenceImage: Data? = nil,
+        similarity: Double? = nil,
+        stylePreset: String? = ""
+    ) {
+        self.prompt = prompt
+        self.referenceImage = referenceImage
+        self.nrOfImages = nrOfImages
+        self.similarity = similarity
+    }
+}
+
+// struct ImageVariationInput: Codable {
+//     let prompt: String
+//     let nrOfImages: Int?
+//     let referenceImage: Data
+//     let similarity: Double?
+
+//     init(
+//         prompt: String,
+//         nrOfImages: Int? = 1,
+//         referenceImage: Data,
+//         similarity: Double? = nil
+//     ) {
+//         self.prompt = prompt
+//         self.referenceImage = referenceImage
+//         self.nrOfImages = nrOfImages
+//         self.similarity = similarity
+//     }
+// }
