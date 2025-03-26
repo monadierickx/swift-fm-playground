f95b25d0a2f92f5e44750f7b78fcd8a1740dcdb2
monadierickx <126071495+monadierickx@users.noreply.github.com>
2025-03-26 18:25:06 +0100

    update SDK

--- backend/Package.swift
+++ backend/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
-        .package(url: "https://github.com/awslabs/aws-sdk-swift", from: "1.2.36"),
+        .package(url: "https://github.com/awslabs/aws-sdk-swift", from: "1.2.45"),
         .package(url: "https://github.com/smithy-lang/smithy-swift", from: "0.118.0"),
         .package(url: "https://github.com/swiftlang/swift-testing", branch: "main"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
