a4c6c0142beac359e67a73138a12d6582c02a9e6
monadierickx <126071495+monadierickx@users.noreply.github.com>
2025-04-11 17:57:02 +0200

    moved libraries to different repository

--- backend/Package.swift
+++ backend/Package.swift
@@ -8,55 +8,21 @@ let package = Package(
     platforms: [.macOS(.v15), .iOS(.v18), .tvOS(.v18)],
     products: [
         .executable(name: "PlaygroundAPI", targets: ["PlaygroundAPI"]),
-        .library(name: "BedrockService", targets: ["BedrockService"]),
-        .library(name: "BedrockTypes", targets: ["BedrockTypes"]),
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
-        .package(url: "https://github.com/awslabs/aws-sdk-swift", from: "1.2.54"),
-        .package(url: "https://github.com/smithy-lang/smithy-swift", from: "0.118.0"),
-        .package(url: "https://github.com/swiftlang/swift-testing", branch: "main"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.5.0"),
+        .package(url: "https://github.com/monadierickx/swift-bedrock-library.git", branch: "main"),
     ],
     targets: [
         .executableTarget(
             name: "PlaygroundAPI",
             dependencies: [
-                .target(name: "BedrockService"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Hummingbird", package: "hummingbird"),
+                .product(name: "BedrockService", package: "swift-bedrock-library"),
             ],
             path: "Sources/PlaygroundAPI"
-        ),
-        .target(
-            name: "BedrockService",
-            dependencies: [
-                .target(name: "BedrockTypes"),
-                .product(name: "AWSClientRuntime", package: "aws-sdk-swift"),
-                .product(name: "AWSBedrock", package: "aws-sdk-swift"),
-                .product(name: "AWSBedrockRuntime", package: "aws-sdk-swift"),
-                .product(name: "Smithy", package: "smithy-swift"),
-                .product(name: "Logging", package: "swift-log"),
-            ],
-            path: "Sources/BedrockService"
-        ),
-        .target(
-            name: "BedrockTypes",
-            dependencies: [
-                .product(name: "AWSBedrockRuntime", package: "aws-sdk-swift"),
-                .product(name: "AWSBedrock", package: "aws-sdk-swift"),
-                .product(name: "Smithy", package: "smithy-swift"),
-            ],
-            path: "Sources/BedrockTypes"
-        ),
-        .testTarget(
-            name: "BedrockServiceTests",
-            dependencies: [
-                .target(name: "BedrockService"),
-                .product(name: "Testing", package: "swift-testing"),
-            ],
-            path: "Tests/BedrockServiceTests"
-        ),
+        )
     ]
 )
